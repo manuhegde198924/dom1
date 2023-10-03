11700a954bec9e5d7b2d56b94b5e01f2d4d2cda8
manoj hegde <manojhegade@gmail.com>
2023-10-03 20:00:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let val = document.getElementById('container');
+
+//change background color
+val.style.backgroundColor = 'Yellow';
+
+//add margin 
+val.style.margin = '20px';
+
+//add padding 
+val.style.padding = '10px';
+
+//change fontsize and font weight
+val.style.fontSize = '18px';
+val.style.fontWeight = 'bold';
+
+//change height and width
+val.style.height = '200px';
+val.style.width = '300px';
+
+//change font color
+val.style.color = 'blue';
